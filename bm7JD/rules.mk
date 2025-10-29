baa8fa79ae083e45c0ee0dd3da99ae75af307caf
hiQeMffQR8fvHAeNdzqM <close.coin7137@fastmail.com>
2025-10-28 20:04:58 -0500

    Move changed line to the top

--- bm7JD/rules.mk
+++ bm7JD/rules.mk
@@ -1,3 +1,4 @@
+SRC += features/sentence_case.c
 CONSOLE_ENABLE = no
 COMMAND_ENABLE = no
 MOUSEKEY_ENABLE = no
@@ -5,4 +6,3 @@ ORYX_ENABLE = yes
 RGB_MATRIX_CUSTOM_KB = yes
 TAP_DANCE_ENABLE = yes
 SPACE_CADET_ENABLE = no
-SRC += features/sentence_case.c
